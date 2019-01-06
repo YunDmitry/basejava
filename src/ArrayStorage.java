2232f9004703587c9edcece9a9862450ef040205
Юн Дмитрий Тимофеевич <dyun@at-consulting.ru>
2019-01-06 16:00:34 +0600

    Драфт 1. Без использования доп. классов типа Arrays или List, возможно не оптимальный.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,53 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for(int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        int i = 0;
+        while(i < size() && storage[i].uuid != uuid) {
+            i++;
+        }
+        return storage[i];
     }
 
     void delete(String uuid) {
+        int i = 0;
+        while(i < size() && storage[i].uuid != uuid) {
+            i++;
+        }
+        if(storage[i].uuid == uuid) {
+            for(int j = i; j < size(); j++) {
+                storage[j] = storage[j+1];
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resultSet = new Resume[size()];
+        for(int i = 0; i < size(); i++) {
+            resultSet[i] = storage[i];
+        }
+        return resultSet;
     }
 
     int size() {
-        return 0;
+        int arrayLength = 0;
+        int i = 0;
+        while(storage[i] != null) {
+            arrayLength++;
+            i++;
+        }
+        return arrayLength;
     }
 }
