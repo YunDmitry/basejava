e6527754c5c95a336a96341f2dbe40353f5938c3
Юн Дмитрий Тимофеевич <dyun@at-consulting.ru>
2019-03-25 20:15:12 +0600

    HW12 draft4: - optimized MainStream methods

--- src/com/dyun/basejava/MainStream.java
+++ src/com/dyun/basejava/MainStream.java
@@ -1,30 +1,41 @@
 package com.dyun.basejava;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class MainStream {
     public static void main(String[] args) {
         int[] values = {1, 7, 2, 7, 2, 1, 1, 5};
         System.out.println(minValue(values));
 
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 12, 45, 21, 1);
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 12, 45, 21, 2);
         System.out.println(oddOrEven(list).toString());
     }
 
     private static int minValue(int[] values) {
-        Stream<Integer> stream = Arrays.stream(values).boxed();
-        return stream.distinct().sorted().reduce((s1, s2) -> s1 * 10 + s2).orElse(0);
+        IntStream intStream = Arrays.stream(values);
+        return intStream.distinct().sorted().reduce((s1, s2) -> s1 * 10 + s2).orElse(0);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        if (integers.stream().reduce((s1, s2) -> s1 + s2).orElse(0) % 2 == 0)
-        {
-            return integers.stream().filter(o -> o % 2 == 0).collect(Collectors.toList());
-        }
-        return integers.stream().filter(o -> o % 2 != 0).collect(Collectors.toList());
+        List<Integer> oddList = new ArrayList<>();
+        List<Integer> evenList = new ArrayList<>();
+        AtomicInteger sum = new AtomicInteger();
+        integers.forEach(item -> {
+            if (item % 2 == 0) {
+                evenList.add(item);
+            } else {
+                oddList.add(item);
+            }
+            sum.addAndGet(item);
+        });
 
+        if (sum.get() % 2 == 0) {
+            return evenList;
+        }
+        return oddList;
     }
 }
