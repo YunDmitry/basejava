37ad4ab2561e073b264aa3d4b53854550232530a
Юн Дмитрий Тимофеевич <dyun@at-consulting.ru>
2019-03-26 10:34:29 +0600

    HW12 draft6: - refactored oddOrEven method

--- src/com/dyun/basejava/MainStream.java
+++ src/com/dyun/basejava/MainStream.java
@@ -1,16 +1,17 @@
 package com.dyun.basejava;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class MainStream {
     public static void main(String[] args) {
         int[] values = {1, 7, 2, 7, 2, 1, 1, 5};
         System.out.println(minValue(values));
 
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 12, 45, 21, 2);
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 12, 45, 21, 1);
         System.out.println(oddOrEven(list).toString());
     }
 
@@ -23,21 +24,14 @@ public class MainStream {
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        List<Integer> oddList = new ArrayList<>();
-        List<Integer> evenList = new ArrayList<>();
         AtomicInteger sum = new AtomicInteger();
-        integers.forEach(item -> {
-            if (item % 2 == 0) {
-                evenList.add(item);
-            } else {
-                oddList.add(item);
-            }
-            sum.addAndGet(item);
-        });
+        Map<Boolean, List<Integer>> groups = integers.stream().collect(
+                Collectors.partitioningBy(
+                        item -> {
+                            sum.addAndGet(item);
+                            return item % 2 == 0;
+                        }));
 
-        if (sum.get() % 2 == 0) {
-            return evenList;
-        }
-        return oddList;
+        return sum.get() % 2 == 0 ? groups.get(true) : groups.get(false);
     }
 }
